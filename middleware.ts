e88d338a9378ac24de9872cc1b6e9dffc99e2450
AbdulMoiz <abdulmoizs2005@gmail.com>
2025-02-17 22:00:56 +0500

    Start integrating Clerk for authentication and authorization

---
+++ middleware.ts
@@ -0,0 +1,33 @@
+// import { NextResponse, type NextRequest } from 'next/server';
+
+// export async function middleware(request: NextRequest) {
+//   const path = request.nextUrl.pathname;
+//   const isDraft = path.startsWith('/Blogs');
+
+//   if (isDraft) {
+//     const authToken = request.cookies.get('auth-token')?.value;
+//     if (!authToken) {
+//       return NextResponse.redirect(new URL('/Home', request.url));
+//     }
+//   }
+
+//   return NextResponse.next();
+// }
+
+// // Apply to all /drafts/* routes
+// export const config = {
+//   matcher: ['/Blogs/:path*'],
+  
+// };
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
